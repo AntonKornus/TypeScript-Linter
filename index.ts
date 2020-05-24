e2be15845f5f91b92646f00bc52c0996c1690aa0
Anton <anton.kornus@gmail.com>
2020-05-24 19:24:48 +0300

    removed project folder

--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
 const core = require('@actions/core');
-const path = require('path');
 const tsLinter = require('tslint');
 
 try {
     const linterAction = (() => {
-        const projectFolder = path.dirname(path.resolve(core.getInput('folder')));
         const configFile = core.getInput('config');
         const rulesFile = core.getInput('rules');
         const options = {fix: false, formatter: 'json'};
 
-        const linterInstance = tsLinter.Linter.createProgram(configFile, projectFolder);
-        console.log(linterInstance.getCurrentDirectory());
+        const linterInstance = tsLinter.Linter.createProgram(configFile);
         const linter = new tsLinter.Linter(options, linterInstance);
         const files = tsLinter.Linter.getFileNames(linterInstance);
         console.log('Found ' + files.length + ' files.');
