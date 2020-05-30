886a04e5f7b0a65d2325e1572466a31bfef6ec11
Anton <anton.kornus@gmail.com>
2020-05-30 19:53:20 +0300

    context.payload

--- index.ts
+++ index.ts
@@ -42,8 +42,7 @@ try {
 
         const context = github.context;
         const pull_request_number = context.payload.pull_request.number;
-        // some test comment
-        console.log(context.payload.pull_request);
+        console.log(context.payload);
         const octokit = new github.GitHub(gitHubToken);
         octokit.issues.createComment({
             ...context.repo,
