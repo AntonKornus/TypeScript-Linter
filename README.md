9e587a219bf92446d73d359ab5b438af32814775
Anton <anton.kornus@gmail.com>
2020-05-30 19:31:20 +0300

    GITHUB_TOKEN

--- README.md
+++ README.md
@@ -16,6 +16,6 @@ jobs:
         id: linter
         uses: AntonKornus/TypeScript-Linter@master
         with:
-          token: ${{ secrets.GITHUB_TOKEN }} #required
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }} #required
           folder: src # optional
 ```
--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const tsLinter = require("tslint");
 const configFile = 'tsconfig.json';
 const rulesFile = 'tslint.json';
 const folder = core.getInput('folder');
-const gitHubToken = core.getInput('token');
+const gitHubToken = core.getInput('GITHUB_TOKEN');
 const options = {fix: false, formatter: 'json'}
 
 try {
